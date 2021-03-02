17ed0140e53cdb03c31dd3ea954ac5aa123ea5a4
Jake Sylvestre <jakesyl@gmail.com>
2021-03-02 03:36:27 -0500

    switch host env

--- examples/main.go
+++ examples/main.go
@@ -32,7 +32,7 @@ func Cmd(args []string) {
 			&cli.StringFlag{
 				Name:  "hostname",
 				Usage: "hostname to start server on",
-				Value: getEnv("host", "localhost"),
+				Value: getEnv("rando", "localhost"),
 			},
 		},
 		Action: func(c *cli.Context) error {
